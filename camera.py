01df201c958edaf63e0be867c63a2b7a52ad1629
mh-anwar <mohammad.anwar2939@gmail.com>
2024-08-01 18:42:06 -0400

    Test basic camera feed

--- camera.py
+++ camera.py
@@ -2,11 +2,13 @@ import cv2
 import sys
 
 
-source = cv2.VideoCapture(0) # use 0 as default device index, instead of using CLI passed args
-win_name = 'Camera Preview'
+source = cv2.VideoCapture(
+    0
+)  # use 0 as default device index, instead of using CLI passed args
+win_name = "Camera Preview"
 cv2.namedWindow(win_name, cv2.WINDOW_NORMAL)
 
-while cv2.waitKey(1) != 27: # Escape
+while cv2.waitKey(1) != 27:  # Escape
     has_frame, frame = source.read()
     if not has_frame:
         break
