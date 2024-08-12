5dd9ff316bc14156991daa0215b1dab33ceead6b
mh-anwar <mohammad.anwar2939@gmail.com>
2024-08-12 15:09:14 -0400

    Almost complete algorithm 1

--- pathPlanning.py
+++ pathPlanning.py
@@ -5,69 +5,80 @@
 import numpy as np
 
 # Input: Map vertex Matrix - 40 by 140m rectangle - "the matrix containing column-wise stacked polygon vertices"
-
-input = np.array([[0, 0, 0, 40, 140], [40, 140, 0, 0, 0]])
+# fmt: off
+input = np.array(
+    [
+        [0, 0, 0, 40, 140], 
+        [40, 140, 0, 0, 0]
+    ]
+)
 # ([[0, 0], [0, 40], [140, 40], [140, 0], [0, 0]])
 # do I use 2x5 or 5x2
+# fmt: on
+dimensions = input.shape
+rows, M = dimensions  # row and columns of input matrix
+print(dimensions)
 
 
 # Output: d*, θ*, Vf
 def main():
+    # Algorithm 1
     # d, θ and Vf are labeled as OUTput for now
-    dOut = 0
-    angleOut = 0
-    vfOut = 0
-    iOut = 0
+    dStar = 0
+    thetaStar = 0
+    Vf = 0
+    iStar = 0
 
     # matlab code index 2 is actually index 1
-    for i in range(len(input) - 1):  # this is messed up?
-        # fmt: off
-        angle = np.arctan2(
-            (input[1, i + 1] - input[1, 0]), 
-            (input[0, i + 1] - input[0, i])
-        )
-        # fmt: on
+    for i in range(M):
 
-        V = np.dot(
-            np.array(
-                [
-                    [np.cos(angle), np.sin(angle)],
-                    [np.sin(-angle), np.cos(angle)],
-                ]
-            ),
+        theta = np.arctan2(
+            (input[1, i + 1] - input[1, i]), (input[0, i + 1] - input[0, i])
+        )
+        rotationMatrix = np.array(
+            [
+                [np.cos(theta), np.sin(theta)],
+                [-np.sin(theta), np.cos(theta)],
+            ]
+        )
+        Ṽ = np.cross(
+            rotationMatrix,
             input,
         )
-        d = np.amax(V[1]) - V[1, i]
-        if i == 1 or d < dOut:
-            dOut = d
-            angleOut = angle
-            iOut = i  # ?
-            print(dOut, angleOut, iOut)
-            break
+        d = np.amax(Ṽ[1]) - Ṽ[1, i]
+        if i == 1 or d < dStar:
+            dStar = d
+            thetaStar = theta
+            iStar = i  # ?
+            print(dStar, thetaStar, iStar)
     Vs = np.array([])
-    if iOut == len(input):  # what is M?
-        # this makes 0 sense "[col(V )M col(V )1 · · · col(V )M ];"
-        for i in range(len(input) - 1):
+    if iStar == M:
+        # this makes 0 sense "[col(Ṽ )M col(Ṽ )1 · · · col(Ṽ )M ];"
+        np.append(Vs, input[:M])
+        for i in range(M):
             col = input[:i]
             np.append(Vs, col)
     else:
         # different Vs
-        for i in range(len(input) - 1):
-            col = input[: iOut + 1]
+        np.append(Vs, input[:iStar])
+        for i in range(M):
+            col = input[: iStar + 1]
             np.append(Vs, col)
-        for i in range(len(input) - 1):
-            col = input[: iOut - 1]
+        np.append(Vs, input[:0])
+        for i in range(M):
+            col = input[: iStar - 1]
             np.append(Vs, col)
-    print(Vs)
-    VPrime = np.dot(
+        np.append(Vs, input[:iStar])
+    VPrime = np.cross(
         np.array(
             [
-                [np.cos(angleOut), np.sin(angleOut)],
-                [np.sin(-angleOut), np.cos(angleOut)],
+                [np.cos(thetaStar), np.sin(thetaStar)],
+                [np.sin(-thetaStar), np.cos(thetaStar)],
             ]
         ),
         Vs,
     )
+    # Algorithm #2
 
 
 if __name__ == "__main__":
